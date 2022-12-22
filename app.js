b68c3fe24f97dd23421e24eaa78e1d7c74ae0726
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-12-22 01:59:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.use(flash())
 
+// middlewares
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash("success_msg")
     res.locals.error_msg = req.flash("error_msg")
